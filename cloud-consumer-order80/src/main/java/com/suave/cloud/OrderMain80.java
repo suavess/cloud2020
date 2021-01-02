c936cd9f4aa4a6e3c7b18bf9c335fd3f2893d2c8
suave <1132888093@qq.com>
2021-01-03 00:38:28 +0800

    Eureka服务注册及服务发现

--- cloud-consumer-order80/src/main/java/com/suave/cloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/suave/cloud/OrderMain80.java
@@ -2,11 +2,13 @@ package com.suave.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Suave
  * @date 2021/1/1 23:54
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
--- cloud-eureka-server7001/src/main/java/com/suave/cloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/suave/cloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.suave.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Suave
- * @date 2020/12/30 19:24
+ * @date 2021/1/3 00:01
  */
-@EnableEurekaClient
 @SpringBootApplication
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
