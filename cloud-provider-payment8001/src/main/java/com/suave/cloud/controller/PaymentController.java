70140469cd9c614457480d8c4b12ca336cce41ee
suave <1132888093@qq.com>
2021-01-06 20:45:50 +0800

    Discovery服务发现

--- cloud-provider-payment8001/src/main/java/com/suave/cloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/suave/cloud/controller/PaymentController.java
@@ -6,7 +6,11 @@ import com.suave.cloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Suave
@@ -21,6 +25,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String port;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -40,6 +47,19 @@ public class PaymentController {
             return new CommonResult<>(200, "查询成功！端口号：" + port, payment);
         }
         return new CommonResult<>(500, "查询失败！端口号：" + port, null);
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("======service:{}", service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info("{}\t{}\t{}\t{}", instance.getServiceId(), instance.getHost(), instance.getPort(), instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
 
