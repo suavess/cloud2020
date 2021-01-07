3d7fe885c187146654e6696898b787762d56ae2a
suave <1132888093@qq.com>
2021-01-07 21:15:13 +0800

    注册进zookeeper

---
+++ cloud-provider-payment-8004/src/main/java/com/suave/cloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.suave.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Suave
+ * @date 2020/12/30 19:24
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment-8004/src/main/java/com/suave/cloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.suave.cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author Suave
+ * @date 2020/12/30 20:29
+ */
+@Slf4j
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/payment/zk")
+    public String create() {
+        return "springcloud with zookeeper" + port + "\t" + UUID.randomUUID().toString();
+    }
+}
+
