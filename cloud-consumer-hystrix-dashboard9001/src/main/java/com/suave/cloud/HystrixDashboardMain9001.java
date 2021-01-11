4043340905529a7a900f1ad9aace6f57a95fb512
suave <1132888093@qq.com>
2021-01-11 20:04:20 +0800

    HystrixDashboard图形化界面监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/suave/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,17 @@
+package com.suave.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Suave
+ * @date 2021/1/11 19:27
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
