50d144941d0214231cbf7bb1b4c455b7b2395d2b
suave <1132888093@qq.com>
2021-01-11 15:11:57 +0800

    服务提供者服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/suave/cloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/suave/cloud/PaymentHystrixMain8001.java
@@ -3,6 +3,7 @@ package com.suave.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @author Suave
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/suave/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/suave/cloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.suave.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -26,7 +28,12 @@ public class PaymentService {
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+            // 当前线程超时时间设置为三秒
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+    })
     public String paymentInfoTimeOut(Integer id) {
+//        int i = 10/0;
         int time = 3;
         try {
             TimeUnit.SECONDS.sleep(time);
@@ -34,5 +41,9 @@ public class PaymentService {
             e.printStackTrace();
         }
         return "线程池: " + Thread.currentThread().getName() + " paymentInfoTimeOut,id: " + id + " 耗时" + time + "秒";
+    }
+
+    public String paymentInfoTimeOutHandler(Integer id) {
+        return "线程池: " + Thread.currentThread().getName() + " 系统繁忙或运行异常，请稍后再试,id: " + id;
     }
 }
