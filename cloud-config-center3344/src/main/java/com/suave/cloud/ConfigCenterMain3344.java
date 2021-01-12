17139bb2bc8e2706ecfba9656118827c8052eb54
suave <1132888093@qq.com>
2021-01-12 15:50:22 +0800

    Config配置中心

---
+++ cloud-config-center3344/src/main/java/com/suave/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.suave.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Suave
+ * @date 2021/1/12 15:24
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
