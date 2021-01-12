8586755ea8c01b6803560f5b2a5adc5a37a91848
suave <1132888093@qq.com>
2021-01-12 20:28:52 +0800

    Bus消息总线配置

---
+++ cloud-config-center3366/src/main/java/com/suave/cloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.suave.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Suave
+ * @date 2021/1/12 16:05
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-center3366/src/main/java/com/suave/cloud/config/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.suave.cloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Suave
+ * @date 2021/1/12 16:07
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
