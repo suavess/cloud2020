aaf2eae534e44c8ba6cc37db0a5788942bf322eb
suave <1132888093@qq.com>
2021-01-12 14:42:14 +0800

    Gateway网关自定义全局过滤器

---
+++ cloud-gateway-gateway9527/src/main/java/com/suave/cloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,33 @@
+package com.suave.cloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @author Suave
+ * @date 2021/1/12 14:30
+ */
+@Configuration
+@Order(0)
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("============ come in MyLogGatewayFilter " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            log.info("============ 用户名为null，非法用户 ==========");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+}
