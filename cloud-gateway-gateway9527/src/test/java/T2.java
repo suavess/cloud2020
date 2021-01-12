fb55e5abb23d9b961c8e7e2daddd2efc1dd5abdb
suave <1132888093@qq.com>
2021-01-12 14:05:12 +0800

    Gateway网关常用断言

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author Suave
+ * @date 2021/1/12 10:57
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
