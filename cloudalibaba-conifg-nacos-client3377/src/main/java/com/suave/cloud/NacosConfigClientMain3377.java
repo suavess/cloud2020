f15207883f7246c7e918a72880d1e969b88fc896
suave <1132888093@qq.com>
2021-01-20 15:47:44 +0800

    nacos服务配置中心

---
+++ cloudalibaba-conifg-nacos-client3377/src/main/java/com/suave/cloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.suave.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Suave
+ * @date 2021/1/20 2:27 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
