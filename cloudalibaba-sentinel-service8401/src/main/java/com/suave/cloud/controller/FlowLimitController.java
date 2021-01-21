0308357fc62138c076026a8e7165b2e3fd988d7c
suave <1132888093@qq.com>
2021-01-22 00:34:36 +0800

    sentinel热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/suave/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/suave/cloud/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.suave.cloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -46,5 +49,17 @@ public class FlowLimitController {
         log.info("testE 测试异常数");
         int age = 10 / 0;
         return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        int age = 10 / 0;
+        return "------testHotKey";
+    }
+
+    public String dealTestHotKey(String p1, String p2, BlockException blockException) {
+        return "------dealTestHotKey";
     }
 }
