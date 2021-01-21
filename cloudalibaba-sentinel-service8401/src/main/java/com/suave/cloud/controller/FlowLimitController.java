be28f5dfa24261210f9eeaf2453eb7c43e2889e2
suave <1132888093@qq.com>
2021-01-22 00:06:02 +0800

    sentinel服务流控降级

--- cloudalibaba-sentinel-service8401/src/main/java/com/suave/cloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/suave/cloud/controller/FlowLimitController.java
@@ -1,21 +1,50 @@
 package com.suave.cloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Suave
  * @date 2021/1/21 7:32 下午
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "----------testA";
     }
 
     @GetMapping("/testB")
     public String testB() {
         return "----------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age = 10 / 0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE() {
+        log.info("testE 测试异常数");
+        int age = 10 / 0;
+        return "------testE 测试异常数";
     }
 }
